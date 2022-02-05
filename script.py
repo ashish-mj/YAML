5de28e0cb89fa0bb5f95f4ecb2f655fa3621c7cc
Ashish M J <ashm.jagadeesh@gmail.com>
2022-02-05 20:34:27 +0530

    Add CRUD methods

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ class crud_operations:
         with open("config.yml", "r") as stream:
             try:
                 self.config = yaml.safe_load(stream)
-                print(self.config)
+                self.collection = self.connect()
             except yaml.YAMLError as exc:
                 print(exc)
                 
@@ -34,10 +34,71 @@ class crud_operations:
         collection = db[database_config["collection-name"]]
         print("Connection Successful")
         return collection
+    
+    def insert(self,id,name,age):
+        try:
+            data = {"_id":id,"name":name,"age":age}
+            self.collection.insert_one(data)
+            print(data)
+        except pymongo.errors.DuplicateKeyError:
+            print("Document exists")
+        
+    def read(self,id):
+        document = self.collection.find_one({'_id':  id})
+        if bool(document)==False:
+            print("ID doesn't exists")
+        else:
+            print(dict(document))
+            
+    def update(self,id,age):
+        if self.collection.count_documents({'_id':  id})==0:
+            print("ID doesn't exist to be updated")
+        else:
+            self.collection.update_one({"_id":id},{ "$set": {"age":age }})
+            print("Document updated")
+        
+            
+    def delete(self,id):
+        if self.collection.count_documents({'_id':  id})==0:
+            print("ID doesn't exist to be deleted")
+        else:
+            data = {"_id":id}
+            self.collection.delete_one(data)
+            print("ID deleted successfully")
+            
+    def interact(self):
+        while(1):
+            print("Select option\n 1 - Insert Document\n2 - Read Document\n3 - Update Document\n4 - Delete Document\nq - Quit\n")
+            n = input()
+            if n=="1":
+                print("Enter ID, Name, Age\n")
+                id,name,age = input().split(" ")
+                self.insert(int(id),name,int(age))
+            elif n=="2":
+                print("Enter ID\n")
+                id = input()
+                self.read(int(id))
+            elif n=="3":
+                print("Enter ID, Age\n")
+                id, age = input().split(" ")
+                self.update(int(id), int(age))
+            elif n=="4":
+                print("Enter ID\n")
+                id = input()
+                self.delete(int(id))
+            elif n=="q":
+                return
+            else:
+                print("Invalid choice")
+            
+            
+        
+        
                 
 
 if __name__=="__main__":
     obj = crud_operations()
-    obj.connect()
+    obj.interact()
+    
                 
 
